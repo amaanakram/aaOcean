683854abad868e9ab5fbaa16a1aedbf9f294615f
Amaan Akram <amaanakram1@gmail.com>
2022-08-05 23:30:11 +0100

    Update aaOceanArnoldShader.cpp
    Compiles with Arnold 7

--- Shaders/Arnold/aaOcean/source/shaders/aaOceanArnoldShader.cpp
+++ Shaders/Arnold/aaOcean/source/shaders/aaOceanArnoldShader.cpp
@@ -55,76 +55,93 @@ enum aaOceanParams
     p_postfix,
     p_currentFrame,
     p_rotateUV,
-    p_transform
+    p_transform,
+    p_spectrum,
+    p_randWeight,
+    p_spectrumMult,
+    p_peakSharpening,
+    p_jswpfetch,
+    p_swell
 };
 
 node_parameters
 {
-    AtMatrix matrix44;
-    AiM4Identity(matrix44);
-    AiParameterVEC ( "uv_coords"        , 1.0f, 1.0f, 1.0f);
-    AiParameterBOOL( "use_uv_input"     , 0);
-    AiParameterFLT ( "fade"             , 0.0f);
-    AiParameterINT ( "resolution"       , 4);
-    AiParameterFLT ( "oceanScale"       , 100.0f);
-    AiParameterFLT ( "oceanDepth"       , 10000.0f);
-    AiParameterFLT ( "surfaceTension"   , 0.0f);
-    AiParameterINT ( "seed"             , 1);
-    AiParameterFLT ( "waveHeight"       , 5.0f);
-    AiParameterFLT ( "velocity"         , 4.5f);
-    AiParameterFLT ( "waveSpeed"        , 1.0f);
-    AiParameterFLT ( "chopAmount"       , 1.0f);
-    AiParameterFLT ( "cutoff"           , 0.0f);
-    AiParameterFLT ( "windDir"          , 45.0f);
-    AiParameterFLT ( "damp"             , 0.985f);
-    AiParameterINT ( "windAlign"        , 0);
-    AiParameterFLT ( "time"             , 0.1f);
-    AiParameterFLT ( "repeatTime"       , 1000.f);
-    AiParameterFLT ( "gamma"            , 1.0f);
-    AiParameterFLT ( "brightness"       , 1.0f);
-    AiParameterBOOL( "raw"              , 0);
-    AiParameterBOOL( "invertFoam"       , 0);
-    AiParameterFLT ( "fMin"             , 0.0f);
-    AiParameterFLT ( "fMax"             , 0.0f);
-    AiParameterBOOL( "writeFile"        , 0);
+    AtMatrix matrix44 = AiM4Identity();
+    AiParameterVec ( "uv_coords"        , 1.0f, 1.0f, 1.0f);
+    AiParameterBool( "use_uv_input"     , 0);
+    AiParameterFlt ( "fade"             , 0.0f);
+    AiParameterInt ( "resolution"       , 4);
+    AiParameterFlt ( "oceanScale"       , 100.0f);
+    AiParameterFlt ( "oceanDepth"       , 10000.0f);
+    AiParameterFlt ( "surfaceTension"   , 0.0f);
+    AiParameterInt ( "seed"             , 1);
+    AiParameterFlt ( "waveHeight"       , 5.0f);
+    AiParameterFlt ( "velocity"         , 4.5f);
+    AiParameterFlt ( "waveSpeed"        , 1.0f);
+    AiParameterFlt ( "chopAmount"       , 1.0f);
+    AiParameterFlt ( "cutoff"           , 0.0f);
+    AiParameterFlt ( "windDir"          , 45.0f);
+    AiParameterFlt ( "damp"             , 0.985f);
+    AiParameterInt ( "windAlign"        , 0);
+    AiParameterFlt ( "time"             , 0.1f);
+    AiParameterFlt ( "repeatTime"       , 1000.f);
+    AiParameterFlt ( "gamma"            , 1.0f);
+    AiParameterFlt ( "brightness"       , 1.0f);
+    AiParameterBool( "raw"              , 0);
+    AiParameterBool( "invertFoam"       , 0);
+    AiParameterFlt ( "fMin"             , 0.0f);
+    AiParameterFlt ( "fMax"             , 0.0f);
+    AiParameterBool( "writeFile"        , 0);
     AiParameterStr ( "outputFolder"     , "");
     AiParameterStr ( "postfix"          , "");
-    AiParameterINT ( "currentFrame"     , 1);
-    AiParameterBOOL( "rotateUV"         , 0);
-    AiParameterMTX ( "transform"        , matrix44);
+    AiParameterInt ( "currentFrame"     , 1);
+    AiParameterBool( "rotateUV"         , 0);
+    AiParameterMtx ( "transform"        , matrix44);
+    AiParameterInt ( "spectrum"         , 0);
+    AiParameterFlt ( "randWeight"       , 1.0f);
+    AiParameterFlt ( "spectrumMult"     , 1.0f);
+    AiParameterFlt ( "peakSharpening"   , 1.0f);
+    AiParameterFlt ( "jswpfetch"        , 1.0f);
+    AiParameterFlt ( "swell"            , 0.0f);
 }
 
 node_update
 {
     // retrieve ocean pointer from user-data
-    aaOcean *pOcean = (aaOcean *)AiNodeGetLocalData(node);
+    aaOcean *pOcean = reinterpret_cast<aaOcean*>(AiNodeGetLocalData(node));
 
     // main input function
     pOcean->input(
-        params[p_resolution].INT,
-        params[p_seed].INT,
-        params[p_oceanScale].FLT,
-        params[p_oceanDepth].FLT,
-        params[p_surfaceTension].FLT,
-        params[p_velocity].FLT,
-        params[p_cutoff].FLT,
-        params[p_windDir].FLT,
-        params[p_windAlign].INT,
-        params[p_damp].FLT,
-        params[p_waveSpeed].FLT,
-        params[p_waveHeight].FLT,
-        params[p_chopAmount].FLT,
-        params[p_time].FLT,
-        params[p_repeatTime].FLT,
-        TRUE);
+        AiNodeGetInt(node, "resolution"),
+        AiNodeGetInt(node, "spectrum"), 
+        AiNodeGetInt(node, "seed"),
+        AiNodeGetFlt(node, "oceanScale"),
+        AiNodeGetFlt(node, "oceanDepth"),
+        AiNodeGetFlt(node, "surfaceTension"),
+        AiNodeGetFlt(node, "velocity"),
+        AiNodeGetFlt(node, "cutoff"),
+        AiNodeGetFlt(node, "windDir"),
+        AiNodeGetInt(node, "windAlign"),
+        AiNodeGetFlt(node, "damp"),
+        AiNodeGetFlt(node, "waveSpeed"),
+        AiNodeGetFlt(node, "waveHeight"),
+        AiNodeGetFlt(node, "chopAmount"),
+        AiNodeGetFlt(node, "time"),
+        AiNodeGetFlt(node, "repeatTime"),
+        TRUE,
+        AiNodeGetFlt(node, "randWeight"),
+        AiNodeGetFlt(node, "spectrumMult"),
+        AiNodeGetFlt(node, "peakSharpening"),
+        AiNodeGetFlt(node, "jswpfetch"),
+        AiNodeGetFlt(node, "swell"));
 
     // see if user has requested normalized or raw foam values
-    bool rawOutput = params[p_raw].BOOL;
+    bool rawOutput = AiNodeGetBool(node, "raw");
     if(pOcean->isChoppy() && !rawOutput)
     {
         float outMin, outMax;
-        float   fmin        = params[p_fMin].FLT;
-        float   fmax        = params[p_fMax].FLT;
+        float   fmin = AiNodeGetFlt(node, "fMin");
+        float   fmax = AiNodeGetFlt(node, "fMax");
         pOcean->getFoamBounds(outMin, outMax);
 
         float epsilon = 1e-3f;
@@ -137,10 +154,10 @@ node_update
 shader_evaluate
 {
     // retrieve ocean pointer from user-data
-    aaOcean *pOcean = (aaOcean*)AiNodeGetLocalData(node);
+    aaOcean* pOcean = reinterpret_cast<aaOcean*>(AiNodeGetLocalData(node));
 
     // get our UV's
-    AtPoint uvPt;
+    AtVector2 uvPt;
     if(AiShaderEvalParamBool(p_useUVInput))
         uvPt = AiShaderEvalParamVec(p_uv_coords);
     else
@@ -150,7 +167,7 @@ shader_evaluate
     }
 
     // retrieve heightfield in world space
-    AtPoint worldSpaceDisplacementVec;
+    AtVector worldSpaceDisplacementVec;
     worldSpaceDisplacementVec.y = pOcean->getOceanData(uvPt.x, uvPt.y, aaOcean::eHEIGHTFIELD);
 
     if(pOcean->isChoppy())
@@ -160,7 +177,7 @@ shader_evaluate
         worldSpaceDisplacementVec.z = pOcean->getOceanData(uvPt.x, uvPt.y, aaOcean::eCHOPZ);
 
         // retrieve foam and store it in our alpha channel
-        sg->out.RGBA.a = pOcean->getOceanData(uvPt.x, uvPt.y, aaOcean::eFOAM);
+        sg->out.RGBA().a = pOcean->getOceanData(uvPt.x, uvPt.y, aaOcean::eFOAM);
 
         // see if user has requested normalized or raw foam values
         if(!AiShaderEvalParamBool(p_raw))
@@ -172,15 +189,15 @@ shader_evaluate
             // fitting to 0 - 1 range using rescale(...)
             // invert result to put foam on wave peaks
             if(AiShaderEvalParamBool(p_invertFoam))
-                sg->out.RGBA.a  = 1.0f - rescale(sg->out.RGBA.a, fmin, fmax, 0.0f, 1.0f);
+                sg->out.RGBA().a = 1.0f - rescale(sg->out.RGBA().a, fmin, fmax, 0.0f, 1.0f);
             else
-                sg->out.RGBA.a  = rescale(sg->out.RGBA.a, fmin, fmax, 0.0f, 1.0f);
+                sg->out.RGBA().a = rescale(sg->out.RGBA().a, fmin, fmax, 0.0f, 1.0f);
 
             // removing negative leftovers
-            sg->out.RGBA.a  = maximum<float>(sg->out.RGBA.a, 0.0f);
+            sg->out.RGBA().a = maximum<float>(sg->out.RGBA().a, 0.0f);
             // apply gamma
-            sg->out.RGBA.a  = pow(sg->out.RGBA.a, AiShaderEvalParamFlt(p_gamma));
-            sg->out.RGBA.a *= AiShaderEvalParamFlt(p_brightness);
+            sg->out.RGBA().a = pow(sg->out.RGBA().a, AiShaderEvalParamFlt(p_gamma));
+            sg->out.RGBA().a *= AiShaderEvalParamFlt(p_brightness);
         }
     }
     else
@@ -188,23 +205,17 @@ shader_evaluate
         // return only heightfield, since ocean isn't choppy, 
         // and set foam to 0.0 since it is derived from choppy oceans
         worldSpaceDisplacementVec.x = worldSpaceDisplacementVec.z = 0.0f;
-        sg->out.RGBA.a = 0.f;
+        sg->out.RGBA().a = 0.f;
     }
 
-    // get space-transform matrix
-    AtMatrix matrix;
-    AiM4Identity(matrix);
-    AiM4Copy(matrix, *AiShaderEvalParamMtx(p_transform));
-
     // convert to the local space of the user-specified transform matrix
-    AtPoint oceanLocalSpace;
-    oceanLocalSpace.x = oceanLocalSpace.z = 0.f;
-    AiM4PointByMatrixMult(&oceanLocalSpace, matrix, &worldSpaceDisplacementVec);
+    AtMatrix transform_matrix = *AiShaderEvalParamMtx(p_transform);
+    AtVector oceanLocalSpace = AiM4PointByMatrixMult(transform_matrix, worldSpaceDisplacementVec);
 
     // store result in output
-    sg->out.RGBA.r = oceanLocalSpace.x;
-    sg->out.RGBA.g = oceanLocalSpace.y;
-    sg->out.RGBA.b = oceanLocalSpace.z;
+    sg->out.RGBA().r = oceanLocalSpace.x;
+    sg->out.RGBA().g = oceanLocalSpace.y;
+    sg->out.RGBA().b = oceanLocalSpace.z;
 }
 
 node_initialize
@@ -219,7 +230,7 @@ node_initialize
 node_finish
 {
     // retrieve ocean pointer from user-data
-    aaOcean *pOcean = (aaOcean *)AiNodeGetLocalData(node);
+     aaOcean* pOcean = reinterpret_cast<aaOcean*>(AiNodeGetLocalData(node));
     
     #ifdef WRITE_OPENEXR
     if(AiNodeGetBool(node, "writeFile"))
